7caf909b56450b6b529fe25750c508ed269b6698
duonghoainam <hoainamum@gmail.com>
2022-12-02 22:55:00 +0700

    fix bug success

--- travel-website-frontend/next.config.js
+++ travel-website-frontend/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+   reactStrictMode: true,
+   swcMinify: true,
+   sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+   },
+   images: {
+      domains: ['images.unsplash.com', 'res.cloudinary.com'],
+   },
+};
+
+module.exports = nextConfig;
--- travel-website-frontend/pages/_app.js
+++ travel-website-frontend/pages/_app.js
@@ -1,7 +1,68 @@
-import '../styles/globals.css'
+import BreakpointProvider from '@components/common/breakpoint/breakpoint-provider';
+import { EmptyLayout } from '@components/layouts/empty-layout';
+import { memo, useEffect } from 'react';
+import gsap from 'gsap';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import MouseFollower from '@components/mouse-follower/mouse-follower';
+import '../scss/main.scss';
 
-export default MyApp
+// AOS styles
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+// SWIPER styles
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+import Magnetic from '@components/mouse-follower/magnetic';
+import { Provider } from 'react-redux';
+import store from '@utils/store';
+
+// Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import MainContext from '@components/common/main-context/main-context';
+
+const MyApp = memo(({ Component, pageProps }) => {
+   const Layout = Component.Layout ?? EmptyLayout;
+
+   useEffect(() => {
+      AOS.init({
+         startEvent: 'DOMContentLoaded',
+         duration: 500,
+         once: true,
+      });
+
+      document.querySelectorAll('[data-magnetic]').forEach(function (el) {
+         new Magnetic(el);
+      });
+
+      return () => {};
+   }, []);
+
+   return (
+      <Provider store={store}>
+         <BreakpointProvider>
+            <MainContext>
+               <Layout>
+                  <Component {...pageProps} />
+               </Layout>
+            </MainContext>
+         </BreakpointProvider>
+
+         <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar
+            newestOnTop
+            draggable={false}
+            pauseOnVisibilityChange
+            closeOnClick
+            pauseOnHover
+         />
+      </Provider>
+   );
+});
+
+MyApp.displayName = 'YOURTOUR';
+export default MyApp;
