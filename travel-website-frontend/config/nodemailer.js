8db3cab9477f01abac01f95fc2d9c3ccdd4bee22
duonghoainam <hoainamum@gmail.com>
2022-12-03 13:35:14 +0700

    send email with nodemaler

--- travel-website-frontend/config/nodemailer.js
+++ travel-website-frontend/config/nodemailer.js
@@ -1,1 +1,18 @@
-import 
+import nodemailer from 'nodemailer'
+
+const email = process.env.EMAIL
+const pass = process.env.EMAIL_APP_PASSWORD
+
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: "ahihimailne@gmail.com",
+        pass: "fpdmfhvrhcfubudc",
+    }
+})
+
+export const mailOptions = {
+    from: "a.com",
+    to: email,
+}
